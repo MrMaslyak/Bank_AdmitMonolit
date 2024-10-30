e9286e15cbdb4efcf339d562eea5ca1d78528391
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-10-30 21:09:10 +0200

    add func button registr

--- src/main/java/org/example/bank/Controller.java
+++ src/main/java/org/example/bank/Controller.java
@@ -2,14 +2,22 @@ package org.example.bank;
 
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import org.example.bank.threads.Time;
 import org.example.bank.webscrap.ExchangeRate;
 import org.example.bank.webscrap.News;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.io.IOException;
 
 public class Controller {
 
 
+    public Button registration  ;
     @FXML
     private Label time, plnValue, euroValue, dollarValue, news1, news2, news3, news4, news5, news6;
 
@@ -40,6 +48,24 @@ public class Controller {
 
 
 
+    }
+
+    public void setRegistration() {
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("registration.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.initStyle(StageStyle.DECORATED);
+
+            registration.getScene().getWindow().hide();
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
