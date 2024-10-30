7db66adc1e321c4b61ca5f2087b04d8286ff6564
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-10-30 22:04:47 +0200

    add new frame - registration and add interface registration

--- src/main/java/org/example/bank/Registration.java
+++ src/main/java/org/example/bank/Registration.java
@@ -1,4 +1,38 @@
 package org.example.bank;
 
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Registration {
+
+    public Button back;
+    public Circle indicatorEmail, indicatorLogin, indicatorPassword;
+
+
+
+
+
+    public void back() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("lobby.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+            back.getScene().getWindow().hide();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
